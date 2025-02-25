9b757cbe2861261ac1aef71ef68e08adcd50eea6
Edmilson <edmilsongomes016@gmail.com>
2025-02-25 01:16:57 -0300

    Incrementação do código e inserção de banco de dados

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-def result():
-    return "Hello, World!"
-print(result())
+import sqlite3
+
+conn = sqlite3.connect("dados.db")
+cursor = conn.cursor()
+
+def create_table():
+    try:
+        conn.execute("CREATE TABLE users(NAME TEXT, AGE INTEGER, EMAIL TEXT)")
+        conn.commit()
+        conn.close()
+        print("Tabela criada com sucessso")
+    except sqlite3.Error as erro:
+        print("Erro em -->" + erro)
+# create_table()
+
+def add():
+    name = input("Digite seu nome : ")
+    age = input("Digite sua idade : ")
+    email = input("Digite seu email : ")
+    try:
+        conn.execute("INSERT INTO users VALUES('"+ name +"', '"+ age +"', '"+ email +"')")
+        conn.commit()
+        conn.close()
+        print("Registro salvo com sucesso")
+    except sqlite3.Error as erro:
+        print("Erro em -->" + erro)
+# add()
+
+def info():
+    result = conn.execute("SELECT * FROM users")
+    print(result.fetchall())
+# info()
